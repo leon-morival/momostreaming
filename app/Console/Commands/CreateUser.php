3abaf1735381c321534f20ee14279eb5e9dd002a
leoncci <leon.morival@gmail.com>
2024-06-24 20:43:43 +0100

    create user command

---
+++ app/Console/Commands/CreateUser.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\User;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class CreateUser extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'user:create 
+                            {name : The name of the user} 
+                            {email : The email address of the user} 
+                            {password : The password for the user}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a new user';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(): void
+    {
+        // Retrieve the input arguments
+        $name = $this->argument('name');
+        $email = $this->argument('email');
+        $password = $this->argument('password');
+
+        // Validate the input data
+        $validator = Validator::make([
+            'name' => $name,
+            'email' => $email,
+            'password' => $password,
+        ], [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8',
+        ]);
+
+        if ($validator->fails()) {
+            $this->error('Validation failed:');
+            foreach ($validator->errors()->all() as $error) {
+                $this->error($error);
+            }
+            return;
+        }
+
+        // Create the user
+        $user = User::create([
+            'name' => $name,
+            'email' => $email,
+            'password' => Hash::make($password),
+        ]);
+
+        if ($user) {
+            $this->info('User created successfully.');
+        } else {
+            $this->error('Failed to create user.');
+        }
+    }
+}
